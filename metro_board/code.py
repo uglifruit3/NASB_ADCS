f927526c80b044e75c1f751328f7691b6c5d9fc2
Miles Zembruski <mszembruski@gmail.com>
2022-10-31 12:01:26 -0400

    updated code.py functionality

--- metro_board/code.py
+++ metro_board/code.py
@@ -3,6 +3,7 @@ import busio
 import time
 import gc
 import adafruit_bno055
+from math import atan2, asin, pow
 
 import test
 
@@ -12,8 +13,6 @@ PRINT_MODE = 1
 #  0 -> newline output (for serial interpretation)
 #  1 -> fancy output with carriage returns (for readability)
 
-# TODO need to verify calibrated sensor accuracies and obtain un-calibrated sensor accuracies
-
 def main():
     print("SYS: DEBUG: " + str(gc.mem_free()) + " bytes free post-load")
 
@@ -22,20 +21,28 @@ def main():
     IMU = test.Inertial_Measurement_Unit(I2C)
     IMU.check_init(I2C)
 
-    IMU.calibrate()
+    #IMU.calibrate()
 
+    if PRINT_MODE == 1:
+        print("")
+        ret_char = "\r"
+        end_char = ""
+    elif PRINT_MODE == 0:
+        ret_char = ""
+        end_char = "\n"
     while True:
         try:
-            data = IMU.euler
+            data = IMU.quaternion
         except OSError:
             print("\nIMU: WARNS: Bad read on attitude register(s).")
             print("IMU: WARNS: Hardware reset initiated by main.")
             IMU.hware_reset()
         else:
-            if PRINT_MODE == 0:
-                print("> {e1:6.2f} {e2:6.2f} {e3:6.2f}".format(e1=data[0], e2=data[1], e3=data[2]))
-            elif PRINT_MODE == 1:
-                print("\r> {e1:6.2f} {e2:6.2f} {e3:6.2f}".format(e1=data[0], e2=data[1], e3=data[2]), end="")
+
+            if len(data) == 3:
+                print("{c1:s}>{e1:06.2f} {e2:06.2f} {e3:06.2f}".format(c1=ret_char, e1=data[0], e2=data[1], e3=data[2]), end=end_char)
+            elif len(data) == 4:
+                print("{c1:s}>{e1:06.3f} {e2:06.3f} {e3:06.3f} {e3:06.3f}".format(c1=ret_char, e1=data[0], e2=data[1], e3=data[2], e4=data[3]), end=end_char)
 
 def disp_I2C_scan(I2C):
     print("I2C: DEBUG: Scanning I2C addresses:")
@@ -46,6 +53,17 @@ def disp_I2C_scan(I2C):
             print(" -> Addresses found:")
             print("    ", end="")
             print([hex(dev_addr) for dev_addr in addrs])
+
+def quat_to_euler(q0, q1, q2, q3):
+    e = [0, 0, 0]
+
+    e[0] = atan2(2*(q0*q1 + q2*q3), 1 - 2*(pow(q1,2)+pow(q2,2)))
+    e[1] = asin(2*(q0*q2 - q3*q1))
+    e[2] = atan2(2*(q0*q3 + q1*q2), 1 - 2*(pow(q2,2)+pow(q3,2)))
+
+    return e
+
+
 
 
 # =================== #
